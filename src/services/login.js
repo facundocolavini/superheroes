a79e9e3475c5aabbe65e055df3b073cedf48edc7
Facundo Colavini <facu_colavini@hotmail.com>
2022-01-06 22:35:25 -0300

    adding style css

--- src/services/login.js
+++ src/services/login.js
@@ -1,17 +1,11 @@
 const ENDPOINT = "https://challenge-react.alkemy.org/"
 
-var apiBase = process.env === 'PRODUCTION' ? 
-    'https://www.productionapp.com/' : 'http://localhost:3000/'
 
 export default function login ({email,password}){
     return fetch(`${ENDPOINT}`,{
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
-        },
-        proxy:{
-            host: apiBase,
-            port: 3000
         },
         body:JSON.stringify({email,password})
     }).then(res => {
